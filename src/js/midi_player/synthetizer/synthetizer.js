0bd831df4ee178b351c3d0e8669e28cbafbe26e9
spessasus <95608008+spessasus@users.noreply.github.com>
2023-06-07 20:27:49 +0200

    Fixed Use Drums Sysex

--- src/js/midi_player/synthetizer/synthetizer.js
+++ src/js/midi_player/synthetizer/synthetizer.js
@@ -306,7 +306,7 @@ export class Synthetizer {
                 if(messageData[2] === 0x42 && messageData[3] === 0x12 && messageData[4] === 0x40)
                 {
                     // 0 means channel 10 (default), 1 means 1 etc.
-                    const channel =[9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15][messageData[5] & 0x0F]; // for example 1A means A = 11
+                    const channel = [9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15][messageData[5] & 0x0F]; // for example 1A means A = 11
                     if(messageData[6] === 0x15) // drum channel
                     {
                         this.midiChannels[channel].percussionChannel = messageData[7] > 0;
