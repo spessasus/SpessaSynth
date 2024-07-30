c796994a0179019623ed0eb2ea7da52930521bff
spessasus <spesekspesek@gmail.com>
2024-07-31 00:58:19 +0200

    add preload

--- src/spessasynth_lib/synthetizer/worklet_system/minify_processor.sh
+++ src/spessasynth_lib/synthetizer/worklet_system/minify_processor.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
 esbuild worklet_processor.js --bundle --minify --format=esm --outfile=../worklet_processor.min.js --platform=browser
 echo "Processor minifed succesfully"
