9b3aa101963f206f56cc3de434967d22ae47e07f
spessasus <spesekspesek@gmail.com>
2024-08-17 19:59:24 +0200

    remove console.log

--- src/website/js/renderer/render.js
+++ src/website/js/renderer/render.js
@@ -26,7 +26,6 @@ export function render(auto = true)
         // draw the individual analysers
         this.renderWaveforms();
     }
-    console.log("r")
 
     if (this.renderNotes && this.noteTimes)
     {
