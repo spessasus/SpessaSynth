c5618cff7882d8372b4afa90ee05170fce236a2d
spessasus <spesekspesek@gmail.com>
2024-09-14 18:34:45 +0200

    Fix audio normalization

--- src/spessasynth_lib/utils/buffer_to_wav.js
+++ src/spessasynth_lib/utils/buffer_to_wav.js
@@ -123,8 +123,8 @@ export function audioBufferToWav(audioBuffer, normalizeAudio = true, channelOffs
     for (let i = 0; i < length; i++)
     {
         // interleave both channels
-        const sample1 = Math.min(1, Math.max(-1, channel1Data[i])) * multiplier;
-        const sample2 = Math.min(1, Math.max(-1,channel2Data[i])) * multiplier;
+        const sample1 = Math.min(32767, Math.max(-37268, channel1Data[i] * multiplier));
+        const sample2 = Math.min(32767, Math.max(-37268, channel2Data[i] * multiplier));
 
         // convert to 16-bit
         wavData[offset++] = sample1 & 0xff;
