841ab03c6e272882d50afeaa44c6ec5b7bac8e6b
spessasus <spesekspesek@gmail.com>
2024-09-23 19:13:37 +0200

    tone down effects a bit more

--- src/spessasynth_lib/synthetizer/worklet_system/worklet_utilities/stereo_panner.js
+++ src/spessasynth_lib/synthetizer/worklet_system/worklet_utilities/stereo_panner.js
@@ -1,5 +1,5 @@
-export const WORKLET_SYSTEM_REVERB_DIVIDER = 1000;
-export const WORKLET_SYSTEM_CHORUS_DIVIDER = 1000;
+export const WORKLET_SYSTEM_REVERB_DIVIDER = 1300;
+export const WORKLET_SYSTEM_CHORUS_DIVIDER = 1300;
 /**
  * stereo_panner.js
  * purpose: pans a given voice out to the stereo output and to the effects' outputs
