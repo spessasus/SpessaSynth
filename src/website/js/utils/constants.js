e011153852dbba318e9d83a3eeebfb8083c2b222
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2024-10-06 19:26:45 +0200

    fix: fix url

--- src/website/js/utils/constants.js
+++ src/website/js/utils/constants.js
@@ -1,7 +1,6 @@
 // import { WORKLET_URL_ABSOLUTE, WORKLET_URL_ABSOLUTE_SOURCE } from 'spessasynth_lib'
-import { WORKLET_URL_ABSOLUTE } from 'spessasynth_lib'
 
-export const WORKLET_WEB_URL = `/src/spessasynth_lib/${WORKLET_URL_ABSOLUTE};`;
+export const WORKLET_WEB_URL = '/website/lib/worklet_processor_main.min.js';
 export const WORKLET_WEB_URL_DEBUG = `/src/spessasynth_lib/synthetizer/worklet_system/worklet_processor_main.js;`
 export const ENABLE_DEBUG = false;
 export const IMPULS_FILE = '/website/lib/impulse_response_2.flac';
