fefcad747b34d5b76f1947e84951bdc3fba2f30f
spessasus <spesekspesek@gmail.com>
2025-03-10 09:31:11 +0100

    worklet processor channel is finally a class

--- src/spessasynth_lib/synthetizer/worklet_system/worklet_utilities/worklet_processor_channel.js
+++ src/spessasynth_lib/synthetizer/worklet_system/worklet_utilities/worklet_processor_channel.js
@@ -1,43 +1,178 @@
 import { CONTROLLER_TABLE_SIZE, CUSTOM_CONTROLLER_TABLE_SIZE, dataEntryStates } from "./controller_tables.js";
 
 /**
- * @typedef {Object} WorkletProcessorChannel
- * @property {Int16Array} midiControllers - array of MIDI controller values + the values used by modulators as the source (pitch bend, bend range etc.).
- * 14-bit values.
- * @property {boolean[]} lockedControllers - array indicating if a controller is locked
- * @property {Float32Array} customControllers -
- * array of custom (not sf2) control values such as RPN pitch tuning, transpose, modulation depth, etc.
- *
- * @property {number} channelTransposeKeyShift - key shift of the channel
- * @property {Int8Array} channelOctaveTuning - the tuning for octave on this channel
- * @property {Int16Array} keyCentTuning - tuning of individual keys in cents
- * @property {boolean} holdPedal - indicates whether the hold pedal is active
- * @property {boolean} drumChannel - indicates whether the channel is a drum channel
- * @property {number} velocityOverride - overrides velocity if > 0 otherwise disabled
- * @property {boolean} randomPan - enables random panning for every note
- *
- * @property {dataEntryStates} dataEntryState - the current state of the data entry
- * @property {number} NRPCoarse - the current coarse value of the Non-Registered Parameter
- * @property {number} NRPFine - the current fine value of the Non-Registered Parameter
- * @property {number} RPValue - the current value of the Registered Parameter
- *
- * @property {number} bank - the channel's bank number
- * @property {BasicPreset} preset - the channel's preset
- * @property {boolean} lockPreset - indicates whether the program on the channel is locked
- * @property {boolean} presetUsesOverride - indcates if the channel uses a preset from the override soundfont.
- *
- * @property {boolean} lockGSNRPNParams - indicates whether the GS NRPN parameters are enabled
- * @property {Object} channelVibrato - vibrato settings for the channel
- * @property {number} channelVibrato.depth - depth of the vibrato effect (cents)
- * @property {number} channelVibrato.delay - delay before the vibrato effect starts (seconds)
- * @property {number} channelVibrato.rate - rate of the vibrato oscillation (Hz)
- 
- * @property {boolean} isMuted - indicates whether the channel is muted
- * @property {WorkletVoice[]} voices - array of voices currently active on the channel
- * @property {WorkletVoice[]} sustainedVoices - array of voices that are sustained on the channel
- * @property {WorkletVoice[][][]} cachedVoices - first is midi note, second is velocity.
- * output is an array of WorkletVoices
+ * This class represents a single MIDI Channel within the synthesizer.
  */
+export class WorkletProcessorChannel
+{
+    /**
+     * An array of MIDI controller values and values used by modulators as the source (e.g., pitch bend, bend range, etc.).
+     * These are stored as 14-bit values.
+     * Refer to controller_tables.js for the index definitions.
+     * @type {Int16Array}
+     */
+    midiControllers = new Int16Array(CONTROLLER_TABLE_SIZE);
+    
+    /**
+     * An array indicating if a controller, at the equivalent index in the midiControllers array, is locked
+     * (i.e., not allowed to change).
+     * A locked controller cannot be modified.
+     * @type {boolean[]}
+     */
+    lockedControllers = Array(CONTROLLER_TABLE_SIZE).fill(false);
+    
+    /**
+     * An array of custom (non-SF2) control values such as RPN pitch tuning, transpose, modulation depth, etc.
+     * Refer to controller_tables.js for the index definitions.
+     * @type {Float32Array}
+     */
+    customControllers = new Float32Array(CUSTOM_CONTROLLER_TABLE_SIZE);
+    
+    /**
+     * The key shift of the channel (in semitones).
+     * @type {number}
+     */
+    channelTransposeKeyShift = 0;
+    
+    /**
+     * An array of octave tuning values for each note on the channel.
+     * Each index corresponds to a note (0 = C, 1 = C#, ..., 11 = B).
+     * @type {Int8Array}
+     */
+    channelOctaveTuning = new Int8Array(12);
+    
+    /**
+     * An array representing the tuning of individual keys in cents.
+     * Each index corresponds to a MIDI note number (0-127).
+     * @type {Int16Array}
+     */
+    keyCentTuning = new Int16Array(128);
+    
+    /**
+     * Indicates whether the sustain (hold) pedal is active.
+     * @type {boolean}
+     */
+    holdPedal = false;
+    
+    /**
+     * Indicates whether this channel is a drum channel.
+     * @type {boolean}
+     */
+    drumChannel = false;
+    
+    /**
+     * If greater than 0, overrides the velocity value for the channel, otherwise it's disabled.
+     * @type {number}
+     */
+    velocityOverride = 0;
+    
+    /**
+     * Enables random panning for every note played on this channel.
+     * @type {boolean}
+     */
+    randomPan = false;
+    
+    /**
+     * The current state of the data entry for the channel.
+     * @type {dataEntryStates}
+     */
+    dataEntryState = dataEntryStates.Idle;
+    
+    /**
+     * The current coarse value of the Non-Registered Parameter (NRPN).
+     * @type {number}
+     */
+    NRPCoarse = 0;
+    
+    /**
+     * The current fine value of the Non-Registered Parameter (NRPN).
+     * @type {number}
+     */
+    NRPFine = 0;
+    
+    /**
+     * The current value of the Registered Parameter (RP).
+     * @type {number}
+     */
+    RPValue = 0;
+    
+    /**
+     * The bank number of the channel (used for patch changes).
+     * @type {number}
+     */
+    bank = 0;
+    
+    /**
+     * The preset currently assigned to the channel.
+     * @type {BasicPreset}
+     */
+    preset = undefined;
+    
+    /**
+     * Indicates whether the program on this channel is locked.
+     * @type {boolean}
+     */
+    lockPreset = false;
+    
+    /**
+     * Indicates whether the channel uses a preset from the override soundfont.
+     * @type {boolean}
+     */
+    presetUsesOverride = false;
+    
+    /**
+     * Indicates whether the GS NRPN parameters are enabled for this channel.
+     * @type {boolean}
+     */
+    lockGSNRPNParams = false;
+    
+    /**
+     * The vibrato settings for the channel.
+     * @type {Object}
+     * @property {number} depth - Depth of the vibrato effect in cents.
+     * @property {number} delay - Delay before the vibrato effect starts (in seconds).
+     * @property {number} rate - Rate of the vibrato oscillation (in Hz).
+     */
+    channelVibrato = { delay: 0, depth: 0, rate: 0 };
+    
+    /**
+     * Indicates whether the channel is muted.
+     * @type {boolean}
+     */
+    isMuted = false;
+    
+    /**
+     * An array of voices currently active on the channel.
+     * @type {WorkletVoice[]}
+     */
+    voices = [];
+    
+    /**
+     * An array of voices that are sustained on the channel.
+     * @type {WorkletVoice[]}
+     */
+    sustainedVoices = [];
+    
+    /**
+     * A 3D array (MIDI note -> velocity -> WorkletVoices) for cached voices for each note and velocity.
+     * @type {WorkletVoice[][][]}
+     */
+    cachedVoices = [];
+    
+    /**
+     * Constructs a new MIDI channel
+     * @param preset {BasicPreset}
+     */
+    constructor(preset)
+    {
+        this.preset = preset;
+        for (let i = 0; i < 128; i++)
+        {
+            this.cachedVoices.push([]);
+        }
+    }
+}
+
 
 /**
  * @param sendEvent {boolean}
@@ -48,40 +183,7 @@ export function createWorkletChannel(sendEvent = false)
     /**
      * @type {WorkletProcessorChannel}
      */
-    const channel = {
-        midiControllers: new Int16Array(CONTROLLER_TABLE_SIZE),
-        lockedControllers: Array(CONTROLLER_TABLE_SIZE).fill(false),
-        customControllers: new Float32Array(CUSTOM_CONTROLLER_TABLE_SIZE),
-        
-        NRPCoarse: 0,
-        NRPFine: 0,
-        RPValue: 0,
-        dataEntryState: dataEntryStates.Idle,
-        
-        voices: [],
-        sustainedVoices: [],
-        cachedVoices: [],
-        preset: this.defaultPreset,
-        presetUsesOverride: false,
-        
-        channelTransposeKeyShift: 0,
-        channelOctaveTuning: new Int8Array(12),
-        keyCentTuning: new Int16Array(128),
-        channelVibrato: { delay: 0, depth: 0, rate: 0 },
-        randomPan: false,
-        velocityOverride: 0,
-        
-        lockGSNRPNParams: false,
-        holdPedal: false,
-        isMuted: false,
-        drumChannel: false,
-        lockPreset: false
-        
-    };
-    for (let i = 0; i < 128; i++)
-    {
-        channel.cachedVoices.push([]);
-    }
+    const channel = new WorkletProcessorChannel(this.defaultPreset);
     this.workletProcessorChannels.push(channel);
     this.resetControllers(this.workletProcessorChannels.length - 1);
     this.sendChannelProperties();
