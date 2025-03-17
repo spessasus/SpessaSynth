99e1f3f8bb0e566ae1dfbe1091bf6034bedf1d75
spessasus <spesekspesek@gmail.com>
2025-03-17 16:22:48 +0100

    improve voice caching

--- src/spessasynth_lib/synthetizer/worklet_system/worklet_methods/soundfont_management/clear_sound_font.js
+++ src/spessasynth_lib/synthetizer/worklet_system/worklet_methods/soundfont_management/clear_sound_font.js
@@ -13,15 +13,11 @@ export function clearSoundFont(sendPresets = true, clearOverride = true)
     }
     this.defaultPreset = this.getPreset(0, 0);
     this.drumPreset = this.getPreset(128, 0);
+    this.cachedVoices = [];
     
     for (let i = 0; i < this.workletProcessorChannels.length; i++)
     {
         const channelObject = this.workletProcessorChannels[i];
-        channelObject.cachedVoices = [];
-        for (let j = 0; j < 128; j++)
-        {
-            channelObject.cachedVoices.push([]);
-        }
         if (!clearOverride || (clearOverride && channelObject.presetUsesOverride))
         {
             channelObject.lockPreset = false;
