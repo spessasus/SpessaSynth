0a9f1c9ccee33565d1471ce01af59cf357eac219
spessasus <spesekspesek@gmail.com>
2025-04-11 21:00:22 +0200

    build scripts movement

--- src/build/minify_website.sh
+++ src/build/minify_website.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")" || exit
 chmod +x minify_processor.sh
 ./minify_processor.sh
 
-
+cd ../website
 esbuild ./js/main/demo_main.js --bundle --minify --sourcemap=linked --format=esm --outfile=minified/demo_main.min.js --platform=browser --external:./externals/libvorbis/encode_vorbis.js
 esbuild css/style.css --bundle --minify --sourcemap=linked --format=esm --outfile=minified/style.min.css --platform=browser
 echo "website minified succesfully"
