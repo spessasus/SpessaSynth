4e2694f83dce040467e72c7d42d40f53a38da8a2
spessasus <spesekspesek@gmail.com>
2025-07-11 01:30:56 +0200

    update core and increase the buffer size

--- src/website/js/manager/custom_synth/worker_thread/synth_worker.js
+++ src/website/js/manager/custom_synth/worker_thread/synth_worker.js
@@ -28,7 +28,7 @@ import { exportSoundBank } from "./export_sf.js";
 
 
 const BLOCK_SIZE = 128;
-const MAX_CHUNKS_QUEUED = 20; // 16 * 128 = 2,048
+const MAX_CHUNKS_QUEUED = 40; // 16 * 128 = 2,048
 
 /*
 This file emulates the worklet_processor.js from spessasynth_lib. However, it also provides an offline rendering method to avoid copying the SF file array buffer, as these can be LARGE.
