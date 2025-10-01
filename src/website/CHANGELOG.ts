20584a3f25d14be7050e59d6151309e3e38831ba
spessasus <spesekspesek@gmail.com>
2025-10-01 14:04:28 +0200

    update libs

--- src/website/CHANGELOG.ts
+++ src/website/CHANGELOG.ts
@@ -1,7 +1,4 @@
 /**
  * Note to self: make sure to update this
  */
-export const WHATS_NEW: string[] = [
-    `Hybrid Audio Engine!`,
-    `Subtitle bugs have been fixed!`
-];
+export const WHATS_NEW: string[] = [`Updated libs`];
