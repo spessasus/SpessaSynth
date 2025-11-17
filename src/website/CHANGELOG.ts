65c67a946924c21194223daf09d5a00383d444db
spessasus <spesekspesek@gmail.com>
2025-11-18 00:40:11 +0100

    update libs

--- src/website/CHANGELOG.ts
+++ src/website/CHANGELOG.ts
@@ -1,4 +1,8 @@
 /**
  * Note to self: make sure to update this
  */
-export const WHATS_NEW: string[] = [`Updated libs`, `Fixed tempo display`];
+export const WHATS_NEW: string[] = [
+    `Updated libs`,
+    `More seamless loops`,
+    `Better custom vibrato`
+];
