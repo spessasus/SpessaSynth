2ea16a401a0312127cd9c2442a8d56abd27260d0
spessasus <spesekspesek@gmail.com>
2026-01-13 23:12:39 +0100

    remove .log from open.ts .log is used for debugging and all of them must be removed before pushing to master

--- src/website/server/open.ts
+++ src/website/server/open.ts
@@ -1,18 +1,26 @@
 import child_process from "node:child_process";
 
 export function openURL(url: string) {
-    switch (process.platform) {
-        case "linux":
-            child_process.exec(`xdg-open ${url}`);
-            break;
-        case "win32":
-            child_process.exec(`start "" ${url}`);
-            break;
-        case "darwin":
-            child_process.exec(`open "${url}"`);
-            break;
-        default:
-            console.log("Could not open the browser. Open the link below:");
-            console.log(url);
+    try {
+        switch (process.platform) {
+            case "linux":
+                child_process.exec(`xdg-open ${url}`);
+                break;
+            case "win32":
+                child_process.exec(`start "" ${url}`);
+                break;
+            case "darwin":
+                child_process.exec(`open "${url}"`);
+                break;
+            default:
+                console.warn(
+                    "Could not open the browser. Open the link below:"
+                );
+                console.info(url);
+        }
+    } catch (e) {
+        console.warn("Could not open the browser:", e);
+        console.info("Open the link below:");
+        console.info(url);
     }
 }
