d5f51680c971f2259b4d48fe9bfa8d2c0c4161cc
spessasus <spesekspesek@gmail.com>
2026-01-31 03:43:33 +0100

    set as 4.1.0

--- src/website/CHANGELOG.ts
+++ src/website/CHANGELOG.ts
@@ -3,5 +3,7 @@
  */
 export const WHATS_NEW: string[] = [
     `Adjustable sample rate!`,
-    `Switching between Chrome and Worklet mode!`
+    `Switching between Chrome and Worklet mode!`,
+    `Added support for poly/mono mode!`,
+    `Fixed visual desync!`
 ] as const;
