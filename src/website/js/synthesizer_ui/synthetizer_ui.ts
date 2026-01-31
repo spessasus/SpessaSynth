b7dc4329fc34d42f5b40fd92613c480311623f84
spessasus <spesekspesek@gmail.com>
2026-02-01 00:44:58 +0100

    fix polymono

--- src/website/js/synthesizer_ui/synthetizer_ui.ts
+++ src/website/js/synthesizer_ui/synthetizer_ui.ts
@@ -1,7 +1,4 @@
-import {
-    hideControllers,
-    showControllers
-} from "./methods/hide_show_controllers.js";
+import { hideControllers, showControllers } from "./methods/hide_show_controllers.js";
 import { toggleDarkMode } from "./methods/toggle_dark_mode.js";
 import { setEventListeners } from "./methods/set_event_listeners.js";
 import { keybinds } from "../utils/keybinds.js";
@@ -25,14 +22,7 @@ import type { Sequencer } from "spessasynth_lib";
 import type { LocaleManager } from "../locale/locale_manager.ts";
 import type { MIDIKeyboard } from "../midi_keyboard/midi_keyboard.ts";
 import { Meter } from "./methods/synthui_meter.ts";
-import {
-    getDrumsSvg,
-    getEmptyMicSvg,
-    getMicSvg,
-    getMuteSvg,
-    getNoteSvg,
-    getVolumeSvg
-} from "../utils/icons.ts";
+import { getDrumsSvg, getEmptyMicSvg, getMicSvg, getMuteSvg, getNoteSvg, getVolumeSvg } from "../utils/icons.ts";
 import { showAdvancedConfiguration } from "./methods/advanced_configuration.ts";
 import { Selector } from "./methods/synthui_selector.ts";
 import type { Synthesizer } from "../utils/synthesizer.ts";
@@ -1305,7 +1295,7 @@ export class SynthetizerUI {
             [channelNumber + 1]
         );
         polyMonoButton.classList.add("controller_element", "mute_button");
-        polyMonoButton.setAttribute("isPoly", "false");
+        polyMonoButton.setAttribute("isPoly", "true");
         polyMonoButton.addEventListener("click", () => {
             this.synth.lockController(
                 channelNumber,
