a3d41b852fceb04a1152820ff06bac0890bb7363
spessasus <spesekspesek@gmail.com>
2026-02-11 02:16:34 +0100

    update libs

--- src/website/changelog.ts
+++ src/website/changelog.ts
@@ -1,7 +1,4 @@
 /**
  * Note to self: make sure to update this
  */
-export const WHATS_NEW: string[] = [
-    `Fixed clicks on rare occasions`,
-    `Support for F5 Multi-Port message on Linux`
-] as const;
+export const WHATS_NEW: string[] = [`Fixed sustain pedal`] as const;
