355cd0af534f59bfa67a7bc5516ca02782fe4e73
spessasus <spesekspesek@gmail.com>
2026-02-14 22:50:08 +0100

    update libs

--- src/website/changelog.ts
+++ src/website/changelog.ts
@@ -2,6 +2,5 @@
  * Note to self: make sure to update this
  */
 export const WHATS_NEW: string[] = [
-    `Added a toggle for dot matrix display`,
-    `Fixed a bug with pausing while no MIDI file is playing`
+    `Fixed a bug with RMIDI DLS files`
 ] as const;
