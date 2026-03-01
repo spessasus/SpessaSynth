a0612b5a3f9b8258e5d5a13f3c3a86ab94642ecb
spessasus <spesekspesek@gmail.com>
2026-03-01 01:42:32 +0100

    tiny fix

--- src/website/js/synthesizer_ui/synthetizer_ui.ts
+++ src/website/js/synthesizer_ui/synthetizer_ui.ts
@@ -1,7 +1,4 @@
-import {
-    hideControllers,
-    showControllers
-} from "./methods/hide_show_controllers.js";
+import { hideControllers, showControllers } from "./methods/hide_show_controllers.js";
 import { toggleDarkMode } from "./methods/toggle_dark_mode.js";
 import { setEventListeners } from "./methods/set_event_listeners.js";
 import { keybinds } from "../utils/keybinds.js";
@@ -26,14 +23,7 @@ import type { Sequencer } from "spessasynth_lib";
 import type { LocaleManager } from "../locale/locale_manager.ts";
 import type { MIDIKeyboard } from "../midi_keyboard/midi_keyboard.ts";
 import { Meter } from "./methods/synthui_meter.ts";
-import {
-    getDrumsSvg,
-    getEmptyMicSvg,
-    getMicSvg,
-    getMuteSvg,
-    getNoteSvg,
-    getVolumeSvg
-} from "../utils/icons.ts";
+import { getDrumsSvg, getEmptyMicSvg, getMicSvg, getMuteSvg, getNoteSvg, getVolumeSvg } from "../utils/icons.ts";
 import { showAdvancedConfiguration } from "./methods/advanced_configuration.ts";
 import { Selector } from "./methods/synthui_selector.ts";
 import type { Synthesizer } from "../utils/synthesizer.ts";
@@ -1584,7 +1574,7 @@ export class SynthetizerUI {
                         );
                     }
                     this.effectControllers.insertion.effectSelector.value =
-                        e.value.toString();
+                        targetEffect.toString();
                     break;
                 }
 
