5d0f93605f7753ffe0f96150a8b2fc975fdfd8eb
spessasus <spesekspesek@gmail.com>
2026-05-31 18:14:03 +0200

    update libs and changelog

--- src/website/changelog.ts
+++ src/website/changelog.ts
@@ -4,7 +4,8 @@ export const UPDATE_NAME = "v4.3.0 The General Update";
  * Note to self: make sure to update this
  */
 export const WHATS_NEW: string[] = [
-    `Overlapping notes!`,
-    `Better Mono Mode!`,
+    `Lockable MIDI Parameters!`,
+    `Velocity Sense Depth!`,
+    `Velocity Sense Offset!`,
     `More bug fixes!`
 ] as const;
