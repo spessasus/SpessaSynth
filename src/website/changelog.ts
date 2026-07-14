15d017e6d48a614479576d0802809a49c4ad8337
spessasus <spesekspesek@gmail.com>
2026-07-15 00:07:35 +0200

    update libraries

--- src/website/changelog.ts
+++ src/website/changelog.ts
@@ -3,9 +3,4 @@ export const UPDATE_NAME = "v4.3.0 The General Update";
 /**
  * Note to self: make sure to update this
  */
-export const WHATS_NEW: string[] = [
-    `Lockable MIDI Parameters!`,
-    `Velocity Sense Depth!`,
-    `Velocity Sense Offset!`,
-    `More bug fixes!`
-] as const;
+export const WHATS_NEW: string[] = [`More bug fixes!`] as const;
