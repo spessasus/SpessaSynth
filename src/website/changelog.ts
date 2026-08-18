98603c1c8b7c39c9cea925b558517372138e20fa
spessasus <spesekspesek@gmail.com>
2026-08-18 14:13:35 +0200

    Update libs to fix bugs

--- src/website/changelog.ts
+++ src/website/changelog.ts
@@ -4,6 +4,7 @@ export const UPDATE_NAME = "v4.3.0 The General Update";
  * Note to self: make sure to update this
  */
 export const WHATS_NEW: string[] = [
-    `Updated French translation!`,
-    `Russian translation by TGaming03!`
+    `Fixed GS port B missing error!`,
+    `Fixed extremely short SF2 loops!`,
+    `Fixed drums on not writing to files!`
 ] as const;
